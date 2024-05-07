03f430b5f466cc81e7e9b8bba888040c1b751146
Shian-Chi <109353604+Shian-Chi@users.noreply.github.com>
2024-05-07 15:26:30 +0800

    Update CSI_H265.py
    Fix prompt error

--- CSI_H265.py
+++ CSI_H265.py
@@ -8,37 +8,56 @@ import gi
 gi.require_version('Gst', '1.0')
 gi.require_version('GstRtspServer','1.0')
 from gi.repository import Gst, GLib, GstRtspServer
+import socket
+
+
+def get_local_ip():
+    # 建立一個臨時的 socket 連接以確定本地 IP
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        # 使用一個不存在的地址 8.8.8.8 進行嘗試連接
+        try:
+            s.connect(('8.8.8.8', 1))
+            IP = s.getsockname()[0]
+        except Exception:
+            IP = '127.0.0.1'
+    return IP
+
+
+port = '8080'
+factory = '/test'
+host = get_local_ip()
 
 # 初始化GStreamer
 Gst.init(None)
 
 # 创建GstRtspServer服务器
 server = GstRtspServer.RTSPServer()
-server.set_service('12345')
+server.set_service(port)
 # 创建GstRTSPMediaFactory
 factory = GstRtspServer.RTSPMediaFactory()
 
+factory.set_launch("nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM),width=720,height=480,framerate=25/1 ! nvvidconv ! nvv4l2h265enc ! h265parse ! rtph265pay name=pay0 pt=96")
 # 開啓 CSI 鏡頭
-factory.set_launch("nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM),width=1280,height=720,framerate=30/1 ! nvvidconv ! nvv4l2h265enc ! h265parse ! rtph265pay name=pay0 pt=96")
 
-# 開啓 USB 鏡頭
 
 #factory.set_launch("v4l2src device=/dev/video1 ! image/jpeg,width=1280,height=720,framerate=30/1 ! jpegdec ! nvvidconv ! nvv4l2h265enc ! h265parse ! rtph265pay name=pay0 pt=96")
+# 開啓 USB 鏡頭
 
 factory.set_shared(True)
 
 # 设置RTSP媒体工厂的挂载点路径
-server.get_mount_points().add_factory("/test", factory)
+server.get_mount_points().add_factory(factory, factory)
 
 # 启动服务器
 server.attach(None)
 
-print("RTSP server is ready at rtsp://192.168.0.161:12345/test")
+print(f"RTSP server is ready at rtsp://{host}:{port}{factory}")
 
 # 设置GLib主循环，以处理GStreamer事件
 main_loop = GLib.MainLoop()
 try:
     main_loop.run()
 except KeyboardInterrupt:
+    pass
     pass
 
